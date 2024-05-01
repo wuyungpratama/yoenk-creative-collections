898c429471e64a183f940218f9586a6f03d8e168
Wuyung Pratama <tamayoenk12@gmail.com>
2024-05-01 18:03:30 +0700

    membuat website marketplace sederhana

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// Toggle class active hamburger
+const NavbarNav = document.querySelector(".Navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  NavbarNav.classList.toggle("active");
+};
+
+// Toggle class active search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active shopping bag
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-bag-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar element
+const hamburger = document.querySelector("#hamburger-menu");
+const searchbutton = document.querySelector("#search-button");
+const shoppingbag = document.querySelector("#shopping-bag-button");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !NavbarNav.contains(e.target)) {
+    NavbarNav.classList.remove("active");
+  }
+  if (!searchbutton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!shoppingbag.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+// // Modal Box
+// const itemDetailModal = document.querySelector("#item-detail-modal");
+// const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+// itemDetailButtons.forEach((btn) => {
+//   btn.onclick = (e) => {
+//     itemDetailModal.style.display = "flex";
+//     e.preventDefault();
+//   };
+// });
+
+// // klik tombol close modal
+// document.querySelector(".modal .close-icon").onclick = (e) => {
+//   itemDetailModal.style.display = "none";
+//   e.preventDefault();
+// };
+
+// // klik tombol diluar modal
+// window.onclick = (e) => {
+//   if (e.target === itemDetailModal) {
+//     itemDetailModal.style.display = "none";
+//   }
+// };
---
+++ src/app.js
@@ -0,0 +1,142 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      {
+        id: 1,
+        name: "HKA Font",
+        img: "HKA Font.svg",
+        price: 15000,
+        description: "Custom font handwriting, hanya ada Regular font",
+      },
+      {
+        id: 2,
+        name: "WPW Font",
+        img: "WPW Font.svg",
+        price: 15000,
+        description: "Custom font handwriting, hanya ada Regular font",
+      },
+      {
+        id: 3,
+        name: "Bussiness Card",
+        img: "Card Template 1.svg",
+        price: 20000,
+        description: "Kartu Nama minimalis dan feminim",
+      },
+      { id: 4, name: "E-Book", img: "E-book.svg", price: 20000 },
+      { id: 5, name: "E-Book", img: "E-book.svg", price: 20000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek jika ada item yg sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // jika belum ada atau cart masih kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang ada, cek apakah barang sama atau beda di cart
+        this.items = this.items.map((item) => {
+          // jika barang beda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang ada, tambah quantity dan total
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yang mau di remove
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // jika item  lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusuri satu-satu
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang diklik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barang sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+// fungsi pencarian
+
+// form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data ketika tombol checkout di klik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/6281334227530?text=" + encodeURIComponent(message));
+});
+
+// format message whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  Phone: ${obj.phone}
+
+Data Pesanan
+${JSON.parse(obj.items).map(
+  (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+)}
+TOTAL: ${rupiah(obj.total)}
+Terima Kasih.`;
+};
+
+// konversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
